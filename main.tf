ace026e9a9074ea8a541eb9ff07a0c1bab2842e5
EC2 Default User <ec2-user@ip-172-31-26-195.ap-northeast-2.compute.internal>
2021-02-25 04:52:29 +0000

    add sg_id

--- main.tf
+++ main.tf
@@ -91,12 +91,17 @@ module "was_sg" {
   vpc_id      = local.vpc_id
 
   # http
-  ingress_rules       = ["http-80-tcp"]
-  ingress_cidr_blocks = ["${module.alb_sg.this_security_group_id}"]
-  # ingress_with_source_security_group_id = ["${module.alb_sg.this_security_group_id}"]
+  # ingress_rules       = ["http-80-tcp"]
+  # ingress_cidr_blocks = ["${module.alb_sg.this_security_group_id}"]
   
-  computed_ingress_cidr_blocks           = ["${local.vpc_cidr_block}"]
-  number_of_computed_ingress_cidr_blocks = 1
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-80-tcp"
+      source_security_group_id = "${module.alb_sg.this_security_group_id}"
+    }
+  ]
+  
+  number_of_computed_ingress_with_source_security_group_id = 1
 }
 
 
